f7abfd9fe5894e63514664795d022eeb9746d6ac
Dark-Hobbit <Arhain@yandex.ru>
2022-10-30 16:48:15 +0000

    Process player stats quick and dirty way

--- data_processing.py
+++ data_processing.py
@@ -52,7 +52,7 @@ class Match:
                 length = 'shootouts'
             else:
                 logger.error(f'Match {self.match_id} of season {self.season_id} has weird length')
-            
+
             return winner, length 
             
         main = bs4.BeautifulSoup(protocol, 'html.parser').find('main')
@@ -66,3 +66,73 @@ class Match:
         score = main.find('div', 'preview-frame__center')
         self.winner, self.length = parse_score(score)
         self.winner = getattr(self, self.winner)
+    
+    def get_player_summary(self, protocol: str) -> None:
+
+        def parse_players_group(players_group: bs4.element.Tag) -> tuple[list, list[list, ...]]:
+#             team_name = players_group.p.text.strip()
+            player_position = players_group.h3.text.strip()
+            table = players_group.table.find_all('tr')
+            header = ['Position']
+            
+            for line in table[0].find_all('th'):
+                if line.title:
+                    header.append(line.title.strip())
+                elif line.text.strip() != 'Player':
+                    header.append(line.text.strip())
+                else:
+                    header.extend(['ID', 'Name'])
+                    
+            stats = []
+            for line in table[1:]:
+                values = [player_position]
+                for cell in line.find_all('th'):
+                    if cell.a:
+                        player_id = cell.a['href'].split('/')[-2]
+                        values.append(player_id)
+                    values.append(cell.text.strip())
+                stats.append(values)
+            
+            return header, stats
+        
+        def parse_players_team(players_team: list[bs4.element.Tag, ...], team_name: str) -> list[Player, ...]:
+            players = []
+            for player_group in players_team:
+                header, stats = parse_players_group(player_group)
+                for line in stats:
+                    player_stats = dict(zip(header, line))
+                    player_stats['Team'] = team_name
+
+                    player_id = player_stats['ID']
+                    player_name = player_stats['Name']
+                    player_position = player_stats['Position']
+                    del player_stats['ID'], player_stats['Name'], player_stats['Position']
+
+                    player = Player(player_id, player_name, player_position)
+                    player.add_match_record(self.season_id, self.match_id, player_stats)
+                    players.append(player)
+            return players
+            
+        
+        main = bs4.BeautifulSoup(protocol, 'html.parser').find('main')
+        home_team_players = main.find_all(class_='wrapper-content mr-30__1280')[:3]
+        away_team_players = main.find_all(class_='wrapper-content mr-30__1280')[3:6]
+        self.home_team_players = parse_players_team(home_team_players, self.home_team)
+        self.away_team_players = parse_players_team(away_team_players, self.away_team)
+        self.parsed = True
+        
+        
+class Player:
+    
+    def __init__(self, player_id: int, name: str, position: str) -> None:
+        self.id = player_id
+        self.name = name
+        self.position = position
+        self.records = {}
+    
+    
+    def add_match_record(self, season_id: int, match_id: int, match_stats: dict[str, int]) -> None:
+        if season_id not in self.records:
+            self.records[season_id] = {}
+        self.records[season_id][match_id] = match_stats
+        
